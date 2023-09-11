c22b0b70839e757248e12e33b496d7123a7e04ef
Muhammad Zubair <muhammad.zubairmz12349@gmail.com>
2023-09-11 11:21:37 +0500

    Dynamic title got added to header section, which will receive title from route(s)

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ var express = require ('express'),
 
     // Landing / Home page
     router.get('/',(req,res) =>{
-      res.render('index.ejs');
+      res.render('index.ejs',{title:"Professional Tax Consultant"});
     })
 
     module.exports = router;
